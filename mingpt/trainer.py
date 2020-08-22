8ba170762417a158f56e3cd98bfd640fb5ed3106
Kamal Raj <kamalraj97@gmail.com>
2020-08-22 16:10:07 +0530

    update trainer

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -56,11 +56,11 @@ class Trainer:
 
         with self.strategy.scope():
             self.model = model(model_config)
-            self.optimizer = AdamWeightDecay(learning_rate=self.lr_cosine_decay,
+            self.optimizer = AdamWeightDecay(learning_rate=self.lr_cosine_decay if config.lr_decay else config.learning_rate,
                                              weight_decay_rate=config.weight_decay,
                                              beta_1=config.betas[0], beta_2=config.betas[1],
                                              gradient_clip_norm=config.grad_norm_clip,
-                                             exclude_from_weight_decay=['layer_norm', 'bias'])
+                                             exclude_from_weight_decay=['layer_normalization', 'bias'])
             self.cce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,reduction=tf.keras.losses.Reduction.NONE)
             self.train_dist_dataset = self.strategy.experimental_distribute_dataset(self.train_dataset)
             if self.test_dataset:
@@ -71,7 +71,9 @@ class Trainer:
             self.model.save_weights(self.config.ckpt_path)
 
     def lr_cosine_decay(self):
-        if self.tokens < self.config.warmup_tokens:
+        if self.tokens == 0:
+            lr_mult = 1
+        elif self.tokens < self.config.warmup_tokens:
             lr_mult = float(self.tokens) / float(max(1, self.config.warmup_tokens))
         else:
             progress = float(self.tokens - self.config.warmup_tokens) / float(max(1, self.config.final_tokens - self.config.warmup_tokens))
@@ -95,7 +97,14 @@ class Trainer:
                 # training=True is only needed if there are layers with different
                 # behavior during training versus inference (e.g. Dropout).
                     logits = self.model(X,training=True)
-                    cross_entropy = self.cce(Y,logits)
+                    num_labels = tf.shape(logits)[-1]
+                    label_mask = tf.math.logical_not(Y < 0)
+                    label_mask = tf.reshape(label_mask,(-1,))
+                    logits = tf.reshape(logits,(-1,num_labels))
+                    logits_masked = tf.boolean_mask(logits,label_mask)
+                    label_ids = tf.reshape(Y,(-1,))
+                    label_ids_masked = tf.boolean_mask(label_ids,label_mask)
+                    cross_entropy = self.cce(label_ids_masked, logits_masked)
                     loss = tf.reduce_sum(cross_entropy) * (1.0 / self.config.batch_size)
 
                 grads = tape.gradient(loss, self.model.trainable_variables)
@@ -116,7 +125,14 @@ class Trainer:
                 # training=True is only needed if there are layers with different
                 # behavior during training versus inference (e.g. Dropout).
                 logits = self.model(X,training=False)
-                cross_entropy = self.cce(Y,logits)
+                num_labels = tf.shape(logits)[-1]
+                label_mask = tf.math.logical_not(Y < 0)
+                label_mask = tf.reshape(label_mask,(-1,))
+                logits = tf.reshape(logits,(-1,num_labels))
+                logits_masked = tf.boolean_mask(logits,label_mask)
+                label_ids = tf.reshape(Y,(-1,))
+                label_ids_masked = tf.boolean_mask(label_ids,label_mask)
+                cross_entropy = self.cce(label_ids_masked, logits_masked)
                 loss = tf.reduce_sum(cross_entropy) * (1.0 / self.config.batch_size)
 
                 return cross_entropy
@@ -134,16 +150,18 @@ class Trainer:
             for epoch in epoch_bar:
                 for inputs in progress_bar(self.train_dist_dataset,total=train_pb_max_len,parent=epoch_bar):
                     loss = train_step(inputs)
-                    self.tokens += tf.reduce_sum(tf.cast(inputs[1]>0,tf.int32)).numpy()
+                    self.tokens += tf.reduce_sum(tf.cast(inputs[1]>=0,tf.int32)).numpy()
                     train_loss_metric(loss)
-                    epoch_bar.child.comment = f'training loss : {train_loss_metric.result()}'
+                    epoch_bar.child.comment = f'training loss : {train_loss_metric.result()} lr : {self.optimizer._decayed_lr(tf.float32):e}'
                 print(f"epoch {epoch+1}: train loss {train_loss_metric.result():.5f}. lr {self.optimizer._decayed_lr(tf.float32):e}")
                 train_loss_metric.reset_states()
 
-            if self.test_dist_dataset:
-                for inputs in progress_bar(self.test_dist_dataset,total=test_pb_max_len,parent=epoch_bar):
-                    loss = test_step(inputs)
-                    test_loss_metric(loss)
-                    epoch_bar.child.comment = f'testing loss : {test_loss_metric.result()}'
-                print(f"epoch {epoch+1}: test loss {test_loss_metric.result():.5f}.")
-                test_loss_metric.reset_states()
+                if self.test_dist_dataset:
+                    for inputs in progress_bar(self.test_dist_dataset,total=test_pb_max_len,parent=epoch_bar):
+                        loss = test_step(inputs)
+                        test_loss_metric(loss)
+                        epoch_bar.child.comment = f'testing loss : {test_loss_metric.result()}'
+                    print(f"epoch {epoch+1}: test loss {test_loss_metric.result():.5f}.")
+                    test_loss_metric.reset_states()
+
+                self.save_checkpoints()
